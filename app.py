de8c89501cee5e52b19de1d4442eb5a0394dc969
Professor Michel Santos <154631555+ProfessorMichelSantos@users.noreply.github.com>
2026-05-13 00:02:31 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,12 +4,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Olá, Azure! 🚀"
-
-
-if __name__ == "__main__":
-    # Azure pode definir a porta por variável de ambiente
-    import os
-    port = int(os.environ.get("PORT", 8000))
-
-    app.run(host="0.0.0.0", port=port)
+    return "AZURE OK FINAL 🚀"
