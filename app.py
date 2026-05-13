bd47c259e4e6a792e0f5b01f6d5165f9d0324848
Professor Michel Santos <154631555+ProfessorMichelSantos@users.noreply.github.com>
2026-05-12 23:45:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 from flask import Flask
 
 app = Flask(__name__)
@@ -6,3 +5,6 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "Olá, Azure!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
