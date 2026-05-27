d0a5f44b8158029fe06bbc4ae9b677a2ffa77ee7
EricAlexandreFinau <ere8611@hotmail.com>
2026-05-27 19:55:38 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,71 +1,12 @@
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
-from typing import Optional
+from flask import Flask, Response
+import os
 
-app = FastAPI()
+app = Flask(__name__)
 
-produtos_db = {}
+@app.route("/")
+def home():
+    return Response("OK AZURE", status=200)
 
-class ProdutoSchema(BaseModel):
-    nome: str
-    preco: float
-    em_estoque: bool
-
-
-# salvando no dicionario
-@app.post("/produtos", status_code=201)
-def criar_produto(produto: ProdutoSchema):
-    novo_id = max(produtos_db.keys()) + 1 if produtos_db else 1
-
-    produtos_db[novo_id] = produto.model_dump()
-
-    return {
-        "id": novo_id,
-        "mensagem": "Produto criado com sucesso"
-    }
-#Atualizar
-@app.put("/produtos/{produto_id}")
-def atualizar_total_produto(produto_id: int, produto_atualizado: ProdutoSchema):
-    if produto_id not in produtos_db:
-        raise HTTPException(status_code=404, detail="Produto não encontrado")
-    
-    produtos_db[produto_id] = produto_atualizado.model_dump()
-
-    return {
-        "mensagem": f"Produto {produto_id} totalmente atualizado",
-        "dados": produtos_db[produto_id]
-    }
-
-class ProdutoPatchSchema(BaseModel):
-    nome: Optional[str] = None
-    preco: Optional[float] = None
-    em_estoque: Optional[bool] = None
-
-#Atualizar parcialmente
-@app.patch("/produtos/{produto_id}")
-def atualizar_parcial_produto(produto_id: int, dados_parciais: ProdutoPatchSchema):
-    if produto_id not in produtos_db:
-        raise HTTPException(status_code=404, detail="Produto não encontrado")
-    
-    produto_atual = produtos_db[produto_id]
-
-    dados_para_atualizar = dados_parciais.model_dump(exclude_unset=True)
-
-    produto_atual.update(dados_para_atualizar)
-
-    return {
-        "mensagem": f"Produto {produto_id} modificado parcialmente",
-        "dados": produto_atual
-    }
-                              
-
-@app.delete("/produtos/{produto_id}")
-def deletar_produto(produto_id: int):
-    if produto_id not in produtos_db:
-        raise HTTPException(status_code=404, detail="Produto não encontrado")
-
-    del produtos_db[produto_id]
-
-    return {
-        "mensagem": f"Produto {produto_id} deletado com sucesso"
-    }
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)  
